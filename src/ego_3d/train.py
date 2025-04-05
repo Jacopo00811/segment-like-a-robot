5d5288c4fd473d3b40d4ce06d25df65c6b7d5114
Jacopo00811 <jacopo.ceccuti@gmail.com>
2025-04-05 15:24:41 +0200

    train file

--- src/ego_3d/train.py
+++ src/ego_3d/train.py
@@ -1,10 +1,17 @@
-from ego_3d.model import Model
-from ego_3d.data import MyDataset
+# from ego_3d.model import Model
+# from ego_3d.data import MyDataset
 
-def train():
-    dataset = MyDataset("data/processed")
-    model = Model()
-    # add rest of your training code here
+# def train():
+#     dataset = MyDataset("data.processed")
+#     model = Model()
+#     # add rest of your training code here
 
-if __name__ == "__main__":
-    train()
+# if __name__ == "__main__":
+#     train()
+
+# Pointcept/configs/scannet/semseg-pt-v3m1-0-base.py
+
+from Pointcept.pointcept.models.point_transformer_v3.point_transformer_v3m1_base import PointTransformerV3
+
+
+model = PointTransformerV3() 
