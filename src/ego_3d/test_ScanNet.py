b57afb799217d8f4fc474fa49e6153347e31a2a4
aka-lowe <akira.miranda.al@gmail.com>
2025-04-07 13:10:42 +0200

    changes to pointcept

---
+++ src/ego_3d/test_ScanNet.py
@@ -0,0 +1,14 @@
+from pointcept.datasets.scannet import ScanNetDataset
+
+
+
+
+dataset_root_path = "/dtu/blackhole/0e/169006/ScanNet"
+
+
+dataset = ScanNetDataset(
+    split="train",
+    data_root=dataset_root_path
+)
+
+print(dataset.get_data_list())
