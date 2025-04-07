9078aaa9b1177f8290bfbab96119f499b5598485
aka-lowe <akira.miranda.al@gmail.com>
2025-04-07 16:55:00 +0200

    Working env for both pointcept and mask3d

--- src/ego_3d/test_ScanNet.py
+++ src/ego_3d/test_ScanNet.py
@@ -1,14 +1,11 @@
 from pointcept.datasets.scannet import ScanNetDataset
 
 
-
-
 dataset_root_path = "/dtu/blackhole/0e/169006/ScanNet/preprocessed"
 
 
 dataset = ScanNetDataset(
-    split="train",
+    split="test",
     data_root=dataset_root_path
 )
 
-print(dataset.get_data_list())
---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+from Mask3d.datasets.preprocessing.scannet_preprocessing import ScannetPreprocessing
+
+
+
+raw_data_dir = '/dtu/datasets2/ScanNet/ScanNetV2'
+preprocessed_data_dir = '/dtu/blackhole/0e/169006/ScanNet/mask3d-preprocessed'
+
