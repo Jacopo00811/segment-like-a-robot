3158920e93218e4f5fba9cf08f04589061215087
aka-lowe <93315804+aka-lowe@users.noreply.github.com>
2025-04-21 22:31:48 +0200

    test

--- src/scannet/eval.py
+++ src/scannet/eval.py
@@ -70,7 +70,7 @@ def eval(cfg, model, val_loader):
             print(f"\nSaved predictions for the last scene: {last_scene_name}")
         elif idx == 1: # Temp for testing
             os.makedirs('reports/scannet/pointcloud', exist_ok=True)
-            last_scene_name = output_dict["scene_name"]
+            last_scene_name = input_dict["scene_name"]
 
             np.save(
                 f"reports/scannet/pointcloud/{last_scene_name}_pred.npy",
