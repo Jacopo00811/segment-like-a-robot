ca80dded5cf135fc0d18c7acdf65e1d784f71aab
aka-lowe <93315804+aka-lowe@users.noreply.github.com>
2025-04-22 00:08:33 +0200

    test

--- src/scannet/eval.py
+++ src/scannet/eval.py
@@ -44,7 +44,7 @@ def build_mod(cfg):
 def eval(cfg, model, val_loader):
 
     target_scene = ""
-
+    print(val_loader)
     for idx, input_dict in enumerate(val_loader):
 
         print("Current scene: ", val_loader.dataset.get_data_name(idx))
