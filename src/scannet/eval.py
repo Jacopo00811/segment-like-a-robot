03422ea3c49a872d6a2c18c3ebcba8b1b67a2506
aka-lowe <93315804+aka-lowe@users.noreply.github.com>
2025-04-21 23:23:16 +0200

    test

--- src/scannet/eval.py
+++ src/scannet/eval.py
@@ -75,8 +75,8 @@ def eval(cfg, model, val_loader):
         intersection, union, target = intersection_and_union_gpu(
             pred,
             segment,
-            num_classes=cfg.data.num_classes,
-            ignore_index=cfg.data.ignore_index,
+            cfg.data.num_classes,
+            cfg.data.ignore_index,
 
         )
 
